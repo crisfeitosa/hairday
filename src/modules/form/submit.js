8e2170d1abeb618b794437905f70e93ceac4ca1f
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-14 11:03:52 -0300

    Enviando um horário selecionado para a API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector('form');
+const clientName = document.getElementById('client');
 const selectedDate = document.getElementById('date');
 
 // Data atual para formatar o input
@@ -14,5 +15,35 @@ form.onsubmit = (event) => {
   // Previne o comportamento padrão de carregar a página
   event.preventDefault();
   
-  console.log("dedfd");
+  try {
+    // Recupera o nome do cliente
+    const name = clientName.value.trim();
+    
+    if(!name) {
+      alert("Informe o nome do cliente!");
+      return;
+    };
+
+    // Recupera o horário selecionado
+    const hourSelected = document.querySelector(".hour-selected");
+    
+    if(!hourSelected) {
+      alert("Selecione a hora.");
+      return;
+    };
+
+    // Recupera somente a hora
+    const [hour] = hourSelected.innerText.split(':');
+
+    // Insere a hora na data
+    const when = dayjs(selectedDate.value).add(hour, 'hour');
+
+    // Gera um ID
+    const id = new Date().getTime();
+
+
+  } catch(error) {
+    alert("Não foi possível enviar o agendamento.");
+    console.log(error);
+  }
 };
