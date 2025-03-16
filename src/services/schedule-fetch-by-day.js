35bd192ab82578e2534e5b6abc2521776b12e1b4
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-03-16 13:27:47 -0300

    Serviço para buscar agendamentos do dia

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,23 @@
+
+import dayjs from 'dayjs';
+import { apiConfig } from './api-config';
+
+export async function scheduleFetchByDay({ date }) {
+  try {
+    // Faz a requisição 
+    const response = await fetch(`${apiConfig.baseURL}/schedule`);
+
+    // Converte para JSON
+    const data = await response.json();
+    
+    // Filtra os agendamentos pelo dia selecionado
+    const dailySchedules = data.filter((schedule) => 
+      dayjs(date).isSame(schedule.when, "day")
+    );
+
+    return dailySchedules;
+  } catch (error) {
+    console.error(error);
+    alert("não foi possível buscar os agendamentos do dia selecionado.");
+  }
+}
